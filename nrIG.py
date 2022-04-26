d9209152ccc7cb167a12bf8d8b4443db57f50de2
n1ghtrid3r <60359484+n1ghtrid3r@users.noreply.github.com>
2022-04-26 01:28:06 +0100

    Add files via upload

--- nrIG.py
+++ nrIG.py
@@ -11,8 +11,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from msedge.selenium_tools import Edge, EdgeOptions
 from selenium.webdriver.support.ui import WebDriverWait
-import datetime
-
+from datetime import datetime
+import pyautogui as pag
+from docutil import DocUtil
 
 
 
@@ -23,7 +24,7 @@ class nrIG:
         self.IGURL = 'https://www.instagram.com/'
         edge_options = EdgeOptions()
         edge_options.use_chromium = True
-        
+
         user_data_dir = r"C:\Users\jasbi\AppData\Local\Microsoft\Edge\User Data\Selenium Dev"
         edge_options.add_argument("user-data-dir={}".format(user_data_dir)); 
 
@@ -35,6 +36,10 @@ class nrIG:
 
         self.browser.get(self.IGURL)
 
+        self.FOLLOW_BUFFER_DURATION = 60
+
+
+
     def ScrollPopupBoxNew(self, pop_up_box, target_element_selector):
         num_elements_found_old = -1
         num_elements_found = 0
@@ -43,36 +48,50 @@ class nrIG:
         sleep(3)
         ActionChains(self.browser).move_to_element(pop_up_box).click().perform()
 
-
-
-
+        found_ig_accounts = []
 
 
         while num_elements_found_old != num_elements_found:
    
             for i in range(0,5):
                 ActionChains(self.browser).move_to_element(pop_up_box).send_keys(Keys.PAGE_DOWN).perform()
-                sleep(0.5)
+                sleep(1)
             tmp = num_elements_found
 
             users_found = self.browser.find_elements(by=By.CSS_SELECTOR, value=target_element_selector)
             num_elements_found = len(users_found)
             num_elements_found_old = tmp
 
+
+            start_index = num_elements_found_old - 1
+            if start_index < 0: start_index = 0
+            end_index = num_elements_found - 1
+            
+            found_ig_accounts_tmp = users_found[start_index: end_index]
+            for found_ig_account in found_ig_accounts_tmp:
+                try:
+                    account_text = found_ig_account.text.upper().split("\n")
+                    user_name = account_text[0]
+                    following_status = account_text[2]
+                    print("FOUND USER {} : {}".format(user_name, following_status))
+                    if following_status != "FOLLOW":
+                        continue
+
+                    follow_button = found_ig_account.find_element(by=By.CSS_SELECTOR, value="div.{}".format("Pkbci button"))
+                    follow_button.click()
+                    sleep(2)
+                    if follow_button.text.upper() == "FOLLOW":
+                        print("LIMIT REACHED")
+                        continue
+                    print("FOLLOWED USER {}".format(user_name))
+                    sleep(self.FOLLOW_BUFFER_DURATION)
+                except Exception as e:
+                    continue
+
             
 
-
-    def ScrollPopupBox(self, popupBox, liCssSelector):
-        numFoundOld = -1
-        numFound = 0
-        while numFound != numFoundOld:
-            for i in range(0,5):
-                self.browser.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;',popupBox)
-                sleep(1)
-            tmp = numFound
-            numFound = len([account for account in self.browser.find_elements_by_css_selector(liCssSelector)])
-            numFoundOld = tmp
-
+            
+ 
 
     
     def ScrollFollowing(self, instagramName):
@@ -90,44 +109,88 @@ class nrIG:
         self.ScrollPopupBoxNew(followersBox, 'div.isgrP ul li')
         return [element.get_attribute("href") for element in self.browser.find_elements(by=By.CSS_SELECTOR, value="a.notranslate._0imsa")]
 
-    def PullData(self, instagramName):
-
-        self.browser.get(self.IGURL + instagramName)
-        links = self.ScrollFollowers()
+    
 
 
 
-
-
-
+    def ActOnFollowers(self, follower_file):
+        print("ACTING ON {}".format(follower_file))
         
-        # found_people_following_statuses = self.browser.find_elements(by=By.CSS_SELECTOR, value="div.{}".format("Pkbci button"))
-        # for following_status_button in found_people_following_statuses:
-        #     following_status_value = following_status_button.find_element(by=By.TAG_NAME, value="div").text.lower()
-        #     if following_status_value == "follow":
-        #         # following_status_button.click()
-        #         print("REQUESTED")
+    
+        FOLLOW_BOT_DELAY_IN_SECONDS = 60
+        ig_accounts = eval(open(follower_file, "r").read())
+        
+        for ig_account in ig_accounts[120:]:
+            self.browser.get(ig_account)
+            try:
+                header = WebDriverWait(self.browser, self.TIMEOUT_DURATION).until(EC.presence_of_element_located((By.TAG_NAME,'header')))
+                follow_button = [button for button in header.find_elements(by=By.CSS_SELECTOR, value='button') if button.text.lower() == 'follow'][0]
+                follow_button.click()
+
+                new_follow_status = follow_button.text.lower()
+                if new_follow_status != "follow":
+                    print("FOLLOWED {}".format(ig_account))
+
+                sleep(FOLLOW_BOT_DELAY_IN_SECONDS)
+            except Exception as e:
+                continue
+
+   
 
 
 IGBot = nrIG()
 
-TARGET_ACCOUNTS = ["bathindiansoc"]
-for target_account in TARGET_ACCOUNTS:
 
-    followers = IGBot.ScrollFollowers(target_account)
-    following = IGBot.ScrollFollowing(target_account)
 
-    time_stamp = datetime.now().strftime("%d/%m/%Y")
-    file_name_followers = "{}_followers_{}.txt".format(target_account, time_stamp)
-    file_name_following = "{}_following_{}.txt".format(target_account, time_stamp)
+def Follow():
+    txt_files = [file for file in DocUtil.ListAllFiles(DocUtil.GetWorkingDirectory()) if DocUtil.GetExtension(file) == ".txt"]
+    for txt_file in txt_files:
+        IGBot.ActOnFollowers(txt_file)
 
-    followers = open(file_name_followers, "w")
-    followers.write(str(followers))
-    followers.close()
+    
 
-    following = open(file_name_following, "w")
-    following.write(str(following))
-    following.close()
+# Follow()
+
+
+
+
+
+def Farm():
+    # DONE_ACCOUNTS = ["bathindiandancesociety","bathindiansoc", "bathtamilsoc"]
+
+    TARGET_ACCOUNTS = ["bathindiandancesociety","bathindiansoc", "bathtamilsoc", "bathhindusoc", "bathmalayaleesoc", "thesubath"]
+
+
+    for target_account in TARGET_ACCOUNTS:
+        print("INITIATING {}".format(target_account))
+        try:
+            followers_list = IGBot.ScrollFollowers(target_account)
+            # time_stamp = datetime.now().strftime("%d%m%Y")
+            # file_name_followers = "{}_followers_{}.txt".format(target_account, time_stamp)
+            # followers = open(file_name_followers, "w")
+            # followers.write(str(followers_list))
+            # followers.close()
+            # print("FOUND {} FOLLOWERS FOR {}".format(str(len(followers_list)), target_account))
+        except Exception as e:
+            print(e)
+        
+        try:
+            following_list = IGBot.ScrollFollowing(target_account)
+            # time_stamp = datetime.now().strftime("%d%m%Y")
+
+            # file_name_following = "{}_following_{}.txt".format(target_account, time_stamp)
+            # following = open(file_name_following, "w")
+            # following.write(str(following_list))
+            # following.close()
+            # print("FOUND {} FOLLOWERS FOR {}".format(str(len(following_list)), target_account))
+
+        except Exception as e:
+            print(e)
+
+        print("DONE_{}".format(target_account))
+
+
+Farm()
 
 
 
